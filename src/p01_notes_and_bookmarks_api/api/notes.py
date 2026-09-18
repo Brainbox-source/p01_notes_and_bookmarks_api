73f7a2f77be44d5c96735a657ded2983576690b1
Brainbox-source <leviprecious701@gmail.com>
2026-09-18 17:58:14 +0100

    build the endpoint to create a note

---
+++ src/p01_notes_and_bookmarks_api/api/notes.py
@@ -0,0 +1,25 @@
+import asyncpg
+from fastapi import APIRouter, HTTPException, Request
+
+from ..db.notes import insert_note
+from ..schemas.note import Note, NoteEntry
+
+# create a router specifically for notes
+router = APIRouter(prefix="/notes", tags=["Notes"])
+
+
+# dependency to get the database pool
+def get_db_pool(request: Request) -> asyncpg.Pool:
+    return request.app.state.db_pool
+
+
+@router.post("/", response_model=Note, status_code=201)
+async def create_note(note_data: NoteEntry, request: Request):
+    """create a new note"""
+    pool = get_db_pool(request)
+
+    try:
+        record = await insert_note(pool, note_data)
+        return record
+    except asyncpg.PostgresError:
+        raise HTTPException(status_code=500, detail="failed to create a note")
